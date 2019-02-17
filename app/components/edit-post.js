0802579a050c283405207b819e25863e02ff8289
Jalaj Khanna <jalaj.khanna@gmail.com>
2019-02-17 16:38:43 -0500

    changes

--- app/components/edit-post.js
+++ app/components/edit-post.js
@@ -10,13 +10,11 @@ export default Component.extend({
       $('.ui.editPost.modal').modal({
         closable: false,
         detachable: false,
-
         onDeny: () => {
           return true;
         },
 
         onApprove: () => {
-          // alert($('#editPostTitle').val());
           this.get('DS').findRecord('post', this.ID).then(function (post) {
             post.set('title', $('#editPostTitle').val());
             post.set('body', $('#editPostBody').val());
